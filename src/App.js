c8fe16043ad9decd305b22f66dba51aee2fab246
SCatella <samuel.catella@gmail.com>
2021-11-23 12:57:32 -0700

    debug server variable

--- src/App.js
+++ src/App.js
@@ -115,7 +115,8 @@ class App extends Component {
         this.setState({ route: route });
     }
 
-    server = toString(process.env.SERVER_ADDRESS);
+    server = 'https://recognibox-api.herokuapp.com/';
+
 
     render() {
         const { isSignedIn, route, box, imageUrl } = this.state;
